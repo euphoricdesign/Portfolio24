426a08ba58151f4ba03d81ef3ba6bce92a2a294b
euphoricdesign <noahchukita@gmail.com>
2023-11-10 15:55:29 -0300

    adding tech stack

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/Navbar/Navbar'
-import Home from './pages/Home'
+import Home from './pages/Home/Home'
 
 
 function App() {
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faHeart, faStar, faFlag } from '@fortawesome/free-solid-svg-icons';
+
+
+library.add(faHeart, faStar, faFlag);
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
